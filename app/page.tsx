00ac27aba73572cc6b83032d3a167dc951bd0d4e
Kenzo Ragundiaz（ラグンディアズ、賢三） <kenzo.ragundiaz18@gmail.com>
2025-08-17 21:53:33 +0800

    feat: random sheesh

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// pages/index.tsx
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>Portfolio... Coming Sooon!!</h1>
+      </main>
+    </div>
+  );
+}
